72998c8266c5ac762b42bab604685c455f91a703
droptheplot <novikov359@gmail.com>
2025-04-08 10:51:13 +0300

    Add release workflow

--- jpackage.sh
+++
@@ -1,6 +0,0 @@
-jpackage --type app-image \
-         --input target/scala-2.13 \
-         --main-jar pingrpc-assembly-0.0.1.jar \
-         --type dmg \
-         --icon src/main/resources/icon.icns \
-         --name PingRPC
