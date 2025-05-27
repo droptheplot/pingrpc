d65e9797d8344259de3b3062f73b7532b21fd320
droptheplot <novikov359@gmail.com>
2025-05-27 09:18:39 +0300

    Disable form on label click

--- src/main/scala/pingrpc/form/FormField.scala
+++ src/main/scala/pingrpc/form/FormField.scala
@@ -4,45 +4,63 @@ import com.google.protobuf.Descriptors
 import com.google.protobuf.Descriptors.FieldDescriptor
 import com.google.protobuf.Descriptors.FieldDescriptor.JavaType
 import io.circe.Json
+import javafx.beans.property.SimpleBooleanProperty
 import javafx.geometry.Pos
-import javafx.scene.Node
+import javafx.scene.{Cursor, Node}
 import javafx.scene.control.{CheckBox, ComboBox, Label, TextField}
 import javafx.scene.layout.HBox
 
+import scala.util.Try
 import scala.util.chaining.scalaUtilChainingOps
 
 case class FormField(fieldDescriptor: FieldDescriptor, node: Node) extends Form {
+  private val isDisabled: SimpleBooleanProperty = new SimpleBooleanProperty(false)
 
   override def toNode: Node = fieldDescriptor.getJavaType match {
     case JavaType.BOOLEAN =>
       new HBox().tap(_.getChildren.add(node))
     case _ =>
+      node.disableProperty.bindBidirectional(isDisabled)
+
+      val label = new Label(fieldDescriptor.getName)
+        .tap(_.setCursor(Cursor.HAND))
+        .tap(_.setOnMouseClicked { _ => isDisabled.set(!isDisabled.get) })
+
       new HBox()
         .tap(_.setSpacing(10))
         .tap(_.setAlignment(Pos.CENTER_LEFT))
-        .tap(_.getChildren.add(new Label(fieldDescriptor.getName)))
+        .tap(_.getChildren.add(label))
         .tap(_.getChildren.add(node))
   }
 
   override def toJson: Json = fieldDescriptor.getJavaType match {
     case JavaType.INT =>
-      parseNumberToJson(fieldDescriptor.getJavaType, node.asInstanceOf[TextField].getText)
+      Try(node.asInstanceOf[TextField]).toOption
+        .filterNot(_.isDisabled)
+        .map(_.getText)
+        .flatMap(parseNumberToJson(fieldDescriptor.getJavaType, _))
         .map(value => Json.obj(fieldDescriptor.getName -> wrapJson(fieldDescriptor, value)))
         .getOrElse(Json.obj())
     case JavaType.BOOLEAN =>
-      Option(node.asInstanceOf[CheckBox].isSelected)
+      Try(node.asInstanceOf[CheckBox]).toOption
+        .filterNot(_.isDisabled)
+        .map(_.isSelected)
         .filter(_ == true)
         .map(_ => Json.obj(fieldDescriptor.getName -> wrapJson(fieldDescriptor, Json.fromBoolean(true))))
         .getOrElse(Json.obj())
     case JavaType.STRING | JavaType.BYTE_STRING | JavaType.LONG | JavaType.DOUBLE | JavaType.FLOAT =>
-      Option(node.asInstanceOf[TextField].getText)
+      Try(node.asInstanceOf[TextField]).toOption
+        .filterNot(_.isDisabled)
+        .map(_.getText)
         .filter(_.nonEmpty)
         .map(value => Json.obj(fieldDescriptor.getName -> wrapJson(fieldDescriptor, Json.fromString(value))))
         .getOrElse(Json.obj())
     case JavaType.ENUM =>
-      val enumValueDescriptor = node.asInstanceOf[ComboBox[Descriptors.EnumValueDescriptor]].getSelectionModel.getSelectedItem
-
-      if (enumValueDescriptor.getIndex == 0) Json.obj()
-      else Json.obj(fieldDescriptor.getName -> wrapJson(fieldDescriptor, Json.fromString(enumValueDescriptor.getName)))
+      Try(node.asInstanceOf[ComboBox[Descriptors.EnumValueDescriptor]]).toOption
+        .filterNot(_.isDisabled)
+        .map(_.getSelectionModel.getSelectedItem)
+        .filterNot(_.getIndex == 0)
+        .map(value => Json.obj(fieldDescriptor.getName -> wrapJson(fieldDescriptor, Json.fromString(value.getName))))
+        .getOrElse(Json.obj())
   }
 }
--- src/main/scala/pingrpc/form/FormMessage.scala
+++ src/main/scala/pingrpc/form/FormMessage.scala
@@ -2,8 +2,9 @@ package pingrpc.form
 
 import com.google.protobuf.Descriptors.FieldDescriptor
 import io.circe.Json
+import javafx.beans.property.SimpleBooleanProperty
 import javafx.geometry.Insets
-import javafx.scene.Node
+import javafx.scene.{Cursor, Node}
 import javafx.scene.control.Label
 import javafx.scene.layout._
 import pingrpc.ui.{boldFont, lightGrayColor}
@@ -12,6 +13,7 @@ import scala.jdk.CollectionConverters._
 import scala.util.chaining.scalaUtilChainingOps
 
 case class FormMessage(fieldDescriptor: FieldDescriptor, children: Seq[Form]) extends Form {
+  private val isDisabled: SimpleBooleanProperty = new SimpleBooleanProperty(false)
 
   override def toNode: Node = {
     val message = new VBox()
@@ -20,22 +22,29 @@ case class FormMessage(fieldDescriptor: FieldDescriptor, children: Seq[Form]) ex
       .tap(_.setBorder(new Border(new BorderStroke(lightGrayColor, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(0, 0, 0, 2)))))
       .tap(_.getChildren.add(new Label(fieldDescriptor.getMessageType.getFullName).tap(_.setFont(boldFont))))
       .tap(_.getChildren.addAll(children.map(_.toNode).asJava))
+      .tap(_.disableProperty.bindBidirectional(isDisabled))
+
+    val label = new Label(fieldDescriptor.getName)
+      .tap(_.setCursor(Cursor.HAND))
+      .tap(_.setOnMouseClicked { _ => isDisabled.set(!isDisabled.get) })
 
     new VBox()
       .tap(_.setSpacing(10))
-      .tap(_.getChildren.add(new Label(fieldDescriptor.getName)))
+      .tap(_.getChildren.add(label))
       .tap(_.getChildren.add(message))
   }
 
   override def toJson: Json =
-    Json.obj {
-      fieldDescriptor.getName ->
-        children
-          .foldLeft(Json.obj()) { case (acc, form) => form.toJson.deepMerge(acc) }
-          .asObject
-          .filter(_.nonEmpty)
-          .map(jsonObject => wrapJson(fieldDescriptor, jsonObject.toJson))
-          .getOrElse(Json.obj())
-          .dropEmptyValues
-    }
+    if (!isDisabled.get) {
+      Json.obj {
+        fieldDescriptor.getName ->
+          children
+            .foldLeft(Json.obj()) { case (acc, form) => form.toJson.deepMerge(acc) }
+            .asObject
+            .filter(_.nonEmpty)
+            .map(jsonObject => wrapJson(fieldDescriptor, jsonObject.toJson))
+            .getOrElse(Json.obj())
+            .dropEmptyValues
+      }
+    } else Json.obj()
 }
